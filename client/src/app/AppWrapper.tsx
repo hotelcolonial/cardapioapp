cedaeab15b60225c9ea88241e260d84c50d22767
Hotel Colonial <colonialiguacufoz@gmail.com>
2024-11-01 15:30:05 -0300

    first commit

---
+++ client/src/app/AppWrapper.tsx
@@ -0,0 +1,16 @@
+import LanguageSelector from "@/components/admin/LanguageSelector";
+import StoreProvider from "./redux";
+
+const AdminLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+const AdminWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <AdminLayout>{children}</AdminLayout>
+    </StoreProvider>
+  );
+};
+
+export default AdminWrapper;
